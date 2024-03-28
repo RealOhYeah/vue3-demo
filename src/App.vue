37c076ff6c816a00b6f9afc08856be14a8eb3564
Oh...Yeah!!! <614988210@qq.com>
2024-03-28 20:01:58 +0800

    action异步实现

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
-<script setup> 
+<script setup>
+import { storeToRefs } from 'pinia'
 import Son1Com from '@/components/Son1Com.vue'
 import Son2Com from '@/components/Son2Com.vue'
-import { useCounterStore } from '@/store/counter.js'  
+import { useCounterStore } from '@/store/counter'
+import { useChannelStore } from './store/channel'
 const counterStore = useCounterStore()
+const channelStore = useChannelStore()
 
-console.log(counterStore);
-
+// 此时，直接解构，不处理，数据会丢失响应式
+const { count, msg } = storeToRefs(counterStore)
+const { channelList } = storeToRefs(channelStore)
+const { getList } = channelStore
 </script>
 
 <template>
-    <h3>App.vue 根组件 - 0
-     -  {{ counterStore.count }}
-     -  {{ counterStore.msg }}
-
+  <div>
+    <h3>
+      App.vue根组件 
+      - {{ count }}
+      - {{ msg }}
     </h3>
     <Son1Com></Son1Com>
     <Son2Com></Son2Com>
-  
+    <hr>
+    <button @click="getList">获取频道数据</button>
+    <ul>
+      <li v-for="item in channelList" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
