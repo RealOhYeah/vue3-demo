20b846861323ba500e8b79e625131899d561faed
Oh...Yeah!!! <614988210@qq.com>
2024-03-28 15:47:21 +0800

    watch代码监听

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,66 @@
-<!-- <script setup>
+<script setup>
+import { ref, watch } from 'vue'
+const count = ref(0)
+const nickname = ref('张三')
 
-import { reactive } from 'vue'
-
-const state = reactive({ 
-  count: 100
- });
-
-const setCount = () => {
-  state.count++;
+const changeCount = () => {
+  count.value++
+}
+const changeNickname = () => {
+  nickname.value = '李四'
 }
 
+// 1. 监视单个数据的变化
+//    watch(ref对象, (newValue, oldValue) => { ... })
+// watch(count, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)
+// })
+
+// 2. 监视多个数据的变化
+//    watch([ref对象1, ref对象2], (newArr, oldArr) => { ... })
+// watch([count, nickname], (newArr, oldArr) => {
+//   console.log(newArr, oldArr)
+// })
+
+// 3. immediate 立刻执行
+// watch(count, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)
+// }, {
+//   immediate: true
+// })
+// --------------------------------------------
+// 4. deep 深度监视, 默认 watch 进行的是 浅层监视
+//    const ref1 = ref(简单类型) 可以直接监视
+//    const ref2 = ref(复杂类型) 监视不到复杂类型内部数据的变化
+const userInfo = ref({
+  name: 'zs',
+  age: 18
+})
+const setUserInfo = () => {
+  // 修改了 userInfo.value 修改了对象的地址，才能监视到
+  // userInfo.value = { name: 'ls', age: 50 }
+  userInfo.value.age++
+}
+
+// deep 开启深度监视(开启后，就可以监视到对象内部值的变化)
+// watch(userInfo, (newValue) => {
+//   console.log(newValue)
+// }, {
+//   deep: true
+// })
+
+// 5. 对于对象中的单个属性，进行监视
+watch(() => userInfo.value.age, (newValue, oldValue) => {
+  console.log(newValue, oldValue)
+})
 </script>
 
 <template>
-  <div>
-    <div>{{ state.count }}</div>
-    <button @click="setCount">+1</button>
-  </div>
-</template>
- -->
- <script setup>
- // 导入
- import { ref } from 'vue'
- // 执行函数 传入参数 变量接收
- const count = ref(1)
- const setCount = ()=>{
-   // 修改数据更新视图必须加上.value
-   count.value*=10
- }
-</script>
-
-<template>
-  <div>{{count}}</div>
-  <button @click="setCount">{{count}}</button>
+  <div>{{ count }}</div>
+  <button @click="changeCount">改数字</button>
+  <div>{{ nickname }}</div>
+  <button @click="changeNickname">改昵称</button>
+  <div>-----------------------</div>
+  <div>{{ userInfo }}</div>
+  <button @click="setUserInfo">修改userInfo</button>
 </template>
